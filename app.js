c0568ebcd836addd2c08922d888428b53e22dc54
Chase Story <chasestory@outlook.com>
2020-04-28 22:59:31 -0600

    added root dir filepath variable

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    res.sendFile(path.join( __dirname, 'views', '404.html'));
+    res.status(404).sendFile(path.join( __dirname, 'views', '404.html'));
     // res.status(404).send('<h1>Page not found! You should probably not be here</h1>');
 });
 
