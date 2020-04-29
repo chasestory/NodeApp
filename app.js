147ccc64f665fcc130f30259cfdec9dc71003025
Chase Story <chasestory@outlook.com>
2020-04-28 21:39:57 -0600

    added admin routes

--- app.js
+++ app.js
@@ -7,11 +7,12 @@ const bodyParser = require("body-parser");
 
 app.use(bodyParser.urlencoded({extended: false}));
 
-app.use(adminRoutes);
-
+app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
-// const server = http.createServer(app);
+app.use((req, res, next) => {
+    res.status(404).send('<h1>Page not found! You should probably not be here</h1>');
+});
 
 const hostname = '127.0.0.1';
 const port = '3000';
