4de446a2b1d9c9849cf03d72e87cafb63dabef82
Chase Story <chasestory@outlook.com>
2020-04-28 20:42:32 -0600

    updated with course changes for app.js, and express.js

--- app.js
+++ app.js
@@ -1,10 +1,25 @@
 const http = require('http');
-const routes = require('./routes');
+const express = require('express');
+// const routes = require('./routes');
+const app = express();
+
+app.use((req, res, next) => {
+    console.log("we are here first");
+    next();
+});
+
+app.use((req, res, next) => {
+    console.log("we are here second");
+    res.send("<h1>Hello From yo momma</h1>");
+});
+
+
+
 
 const hostname = '127.0.0.1';
 const port = '3000';
 
-const server = http.createServer(routes);
+const server = http.createServer(app);
 
 server.listen(port, hostname, () => {
     console.log(`Server running at http://${hostname}:${port}/`);
